3d6d1079c6d7dd9bbfe52625e76e3155b5699874
Rayckey <wwang79@jhu.edu>
2019-06-27 13:31:17 +0800

    yo yo yo with c

---
+++ cII.cpp
@@ -0,0 +1,19 @@
+﻿#include "innfosimu.h"
+
+
+extern "C"
+{
+
+    INNFOSIMU* IMU_ptr=NULL;
+
+
+    void initIMU(){
+        IMU_ptr = new INNFOSIMU();
+    }
+
+    INNFOSIMU::Quaternion ReceiveQue(int id){
+        return IMU_ptr->ReceiveQue(id);
+    }
+
+}
+
